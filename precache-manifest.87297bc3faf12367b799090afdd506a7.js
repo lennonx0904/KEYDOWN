846356b697dfc19e884ae0d1176b0ec2d65edd74
LennonX0904 <LennonX0904@gmail.com>
2019-05-06 13:37:57 +0800

    Updates

---
+++ precache-manifest.87297bc3faf12367b799090afdd506a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "130cf9620873ca85cd7be71c9648443e",
+    "url": "/KEYDOWN/static/media/dj.130cf962.jpg"
+  },
+  {
+    "revision": "384a3e1a9fbe1abecf86",
+    "url": "/KEYDOWN/static/js/runtime~main.7cadf697.js"
+  },
+  {
+    "revision": "cebb8dde7591fd29bdef",
+    "url": "/KEYDOWN/static/js/main.413d74bd.chunk.js"
+  },
+  {
+    "revision": "f9acc58642e729b0efb3",
+    "url": "/KEYDOWN/static/js/2.a54cf656.chunk.js"
+  },
+  {
+    "revision": "cebb8dde7591fd29bdef",
+    "url": "/KEYDOWN/static/css/main.205d2fbd.chunk.css"
+  },
+  {
+    "revision": "981f6ba132e1e6ade8b0c5b92d442ee0",
+    "url": "/KEYDOWN/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/KEYDOWN/precache-manifest.506ef6708d46f71557dfa304b1f03e31.js"
+  "/KEYDOWN/precache-manifest.87297bc3faf12367b799090afdd506a7.js"
 );
 
 workbox.clientsClaim();
